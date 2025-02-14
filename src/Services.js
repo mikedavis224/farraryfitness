54d7f124a1dc57a07386202bbde8621ea2b14af2
Nugo Tech <mldavis224@hotmail.com>
2025-02-13 20:33:35 -0500

    updated bg color

--- src/Services.js
+++ src/Services.js
@@ -2,7 +2,7 @@ import React from "react";
 
 const Services = () => {
   return (
-    <div className="bg-[#016162] py-20">
+    <div className="bg-[#B80500] py-20">
       <div className="container mx-auto">
         <h2 className="text-4xl font-bold text-center text-white mb-10">
           Our Services
