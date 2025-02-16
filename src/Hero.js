af920e68fe10b0a3e976726a6cd7f6442152be21
Nugo Tech <mldavis224@hotmail.com>
2025-02-16 13:45:47 -0500

    add bg video

--- src/Hero.js
+++ src/Hero.js
@@ -11,7 +11,7 @@ const Hero = () => {
         playsInline
         className="absolute top-0 left-0 w-full h-full object-cover"
       >
-        <source src="/background-video.mp4" type="video/mp4" />
+        <source src="/public/background-video.mp4" type="video/mp4" />
         Your browser does not support the video tag.
       </video>
 
